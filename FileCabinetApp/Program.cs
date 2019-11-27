59bbfd9bd04c2a95364fe7155464db112c8bdfec
adrowsychild <a.drowsy.child@gmail.com>
2019-11-27 18:56:42 +0300

    Add initial version of FileCabinetApp.

---
+++ FileCabinetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetApp
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

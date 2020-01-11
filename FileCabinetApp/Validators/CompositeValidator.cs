d742ffb195433239b0d9b18fd76e5ffb6c64fa29
adrowsychild <a.drowsy.child@gmail.com>
2020-01-11 13:48:00 +0300

    Add ValidatorBuilder.

--- FileCabinetApp/Validators/CompositeValidator.cs
+++ FileCabinetApp/Validators/CompositeValidator.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 
 namespace FileCabinetApp.Validators
 {
-    public abstract class CompositeValidator : IRecordValidator
+    /// <summary>
+    /// Composite validator.
+    /// </summary>
+    public class CompositeValidator : IRecordValidator
     {
         private readonly List<IRecordValidator> validators;
 
-        protected CompositeValidator(IEnumerable<IRecordValidator> validators)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositeValidator"/> class.
+        /// </summary>
+        /// <param name="validators">List of validators.</param>
+        public CompositeValidator(IEnumerable<IRecordValidator> validators)
         {
             this.validators = (List<IRecordValidator>)validators;
         }
 
+        /// <summary>
+        /// Validates user's input.
+        /// </summary>
+        /// <param name="record">Record to validate.</param>
+        /// <returns>Whether record is valid.</returns>
         public bool Validate(FileCabinetRecord record)
         {
             bool isValid = false;
---
+++ FileCabinetApp/Validators/ValidatorBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp.Validators
+{
+    public class ValidatorBuilder
+    {
+        private readonly List<IRecordValidator> validators;
+
+        public ValidatorBuilder ValidateFirstName(int min, int max)
+        {
+            this.validators.Add(new FirstNameValidator(min, max));
+
+            return this;
+        }
+
+        public ValidatorBuilder ValidateLastName(int min, int max)
+        {
+            this.validators.Add(new LastNameValidator(min, max));
+
+            return this;
+        }
+
+        public ValidatorBuilder ValidateDateOfBirth(int from, int to)
+        {
+            this.validators.Add(new FirstNameValidator(from, to));
+
+            return this;
+        }
+
+        public ValidatorBuilder ValidateFavouriteNumber(short minValue, short maxValue)
+        {
+            this.validators.Add(new FavouriteNumberValidator(minValue, maxValue));
+
+            return this;
+        }
+
+        public ValidatorBuilder ValidateFavouriteCharacter(int symbolCase)
+        {
+            this.validators.Add(new FavouriteCharacterValidator(symbolCase));
+
+            return this;
+        }
+
+        public ValidatorBuilder ValidateFavouriteGame(int min, int max)
+        {
+            this.validators.Add(new FavouriteGameValidator(min, max));
+
+            return this;
+        }
+
+        public ValidatorBuilder ValidateDonations(int minValue)
+        {
+            this.validators.Add(new DonationsValidator(minValue));
+
+            return this;
+        }
+
+        public IRecordValidator Create()
+        {
+            return new CompositeValidator(this.validators);
+        }
+    }
+}
